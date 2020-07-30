17a83663b80d70a1a7d4d3e5c2e0af49b76d9ef0
mgs95 <marianogs_95@hotmail.com>
2020-07-30 16:23:46 +0000

    Deploying to gh-pages from  @ 4bc0cbd0acaf47071ce6a3405ec82d9fc56bb46b 🚀

---
+++ component---src-pages-404-js-834f5a003943d3ca940f.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[35],{w2l6:function(t,e,n){"use strict";n.r(e);var i=n("xH0s");e.default=function(){return Object(i.c)("div",{style:{display:"flex",alignItems:"center",justifyContent:"center",height:"100vh",width:"100vw",fontSize:32}},"Not Found")}}}]);
+//# sourceMappingURL=component---src-pages-404-js-834f5a003943d3ca940f.js.map

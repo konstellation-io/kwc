dc163541ae8fafa98562953fecefbe3ec8af5217
Mariano Gonzalez Salazar <mariano.gonzalez@intelygenz.com>
2020-11-25 16:58:31 +0100

    Expandable text input added and minor fixes included

--- dev-env/src/App.tsx
+++ dev-env/src/App.tsx
@@ -1,19 +1,17 @@
 import 'kwc/dist/index.css';
-import 'kwc/dist/styles/app.global.scss';
+import 'kwc/src/styles/app.global.scss';
 
-import CloseIcon from '@material-ui/icons/Close';
+import { ExpandableTextInput, Select, SelectTheme } from 'kwc';
+
+import EmailIcon from '@material-ui/icons/Email';
 import React from 'react';
-import { TextInput } from 'kwc';
 import styles from './App.module.scss';
 
 function App() {
   return (
     <div className={styles.App}>
       <header className={styles.header}>
-        <TextInput
-          label="Some label"
-          Icon={ CloseIcon }
-          placeholder="bla bla bla"
+        <ExpandableTextInput
         />
       </header>
     </div>
---
+++ packages/konstellation-web-components/src/components/Form/ExpandableTextInput/ExpandableTextInput.tsx
@@ -0,0 +1,70 @@
+import React, {
+  ChangeEvent,
+  KeyboardEvent,
+  useEffect,
+  useState,
+} from 'react';
+
+import IconSearch from '@material-ui/icons/Search';
+import cx from 'classnames';
+import styles from './ExpandableTextInput.module.scss';
+
+export type ExpandableTextInputProps = {
+  onEnterKeyPress?: Function;
+  onChange?: Function;
+  onBlur?: Function;
+  formValue?: string | number;
+  className?: string;
+};
+
+export function ExpandableTextInput({
+  onEnterKeyPress = function () {},
+  onChange = function () {},
+  onBlur = function () {},
+  formValue = '',
+  className = '',
+}: ExpandableTextInputProps) {
+  const [value, setValue] = useState(formValue);
+  const [opened, setOpened] = useState(false);
+
+  useEffect(() => {
+    setValue(formValue);
+  }, [formValue, setValue]);
+
+  function updateValue(newValue: string) {
+    setValue(newValue);
+    onChange(newValue);
+  }
+
+  function onType(e: ChangeEvent<HTMLInputElement>) {
+    updateValue(e.target.value);
+  }
+
+  function onKeyPress(e: KeyboardEvent<HTMLInputElement>) {
+    e.key === 'Enter' && onEnterKeyPress();
+  }
+
+  function onInputBlur() {
+    onBlur();
+    setOpened(false);
+  }
+
+  return (
+    <div
+      className={cx(styles.container, className, {
+        [styles.opened]: opened,
+      })}
+      onClick={() => !opened && setOpened(true)}
+    >
+      <input
+        className={styles.input}
+        value={value}
+        type="text"
+        onChange={onType}
+        onKeyPress={onKeyPress}
+        onBlur={() => onInputBlur()}
+      />
+      <IconSearch className={cx("icon-regular", styles.searchIcon)}/>
+    </div>
+  );
+}

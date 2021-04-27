024013a74ed377f10139579ef2a102d94fd083f9
Mariano Gonzalez Salazar <mariano.gonzalez@intelygenz.com>
2021-04-27 11:23:50 +0200

    fix: styles on github page

--- packages/docs/doczrc.js
+++ packages/docs/doczrc.js
@@ -1,7 +1,15 @@
+import { css } from 'docz-plugin-css';
+
 export default {
   typescript: true,
   base: '/kwc/',
   src: './src/docs',
   title: 'Konstellation Web Components',
   menu: ['Readme', 'Design', 'Layout', 'Components', 'Forms', 'Hooks'],
+  plugins: [
+    css({
+      preprocessor: 'sass',
+      cssmodules: true
+    })
+  ]
 };

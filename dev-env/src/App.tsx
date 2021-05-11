9cdc02f774d8a9422fcad429aa5a78d0829a183e
Mariano Gonzalez Salazar <mariano.gonzalez@intelygenz.com>
2021-05-11 16:07:12 +0200

    fix: add fix comment

--- dev-env/src/App.tsx
+++ dev-env/src/App.tsx
@@ -1,31 +1,35 @@
 import 'kwc/dist/index.css';
 import 'kwc/src/styles/app.global.scss';
 
-import { Button, CHECK, TextInput } from 'kwc';
+import { Button, CHECK, TextInput, Select } from 'kwc';
 import React, { useEffect, useState } from 'react';
 
 import EmailIcon from '@material-ui/icons/Email';
 import styles from './App.module.scss';
 
 function App() {
-  const [p, setP] = useState<boolean>(false);
-
-
-  useEffect(() => {
-    setTimeout(() => {
-      setP(true);
-    }, 2000)
-  }, [])
+  const [p, setP] = useState<string>('Option Long A');
 
   return (
     <div className={styles.App}>
       <header className={styles.header}>
-        <Button
-          label="SOMETHING"
-          loading
-          primary
+        <Button label="SOMETHING" loading primary />
+        <Select
+          options={['Option Long A', 'Op B', 'Opcion C']}
+          valuesMapper={{
+            ['Option Long A']: 'OPTION LONG A',
+            ['Op B']: 'OP B',
+            ['Opcion C']: 'OPCION C',
+          }}
+          height={30}
+          hideError
+          formSelectedOption={p}
+          onChange={(opt: any) => {
+            setTimeout(() => {
+              setP(opt);
+            }, 2000)
+          }}
         />
-        <TextInput label="some" formValue="asdadasa" hidden />
       </header>
     </div>
   );

c4554e03d89f2f765d4763136ad354289777f437
mgs95 <marianogs_95@hotmail.com>
2021-05-24 16:49:30 +0000

    Deploying to gh-pages from  @ edb73c7866931f12d7ad5fcbfeb88247c9ea4d67 🚀

---
+++ component---src-pages-404-js-aa8360c40d0857254d61.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[39],{w2l6:function(t,e,n){"use strict";n.r(e);var i=n("xH0s");e.default=function(){return Object(i.c)("div",{style:{display:"flex",alignItems:"center",justifyContent:"center",height:"100vh",width:"100vw",fontSize:32}},"Not Found")}}}]);
+//# sourceMappingURL=component---src-pages-404-js-aa8360c40d0857254d61.js.map
